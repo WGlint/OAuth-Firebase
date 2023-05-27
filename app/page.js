8caeed6456af319819d174bfc1590105e2f02b4c
WGlint <a.elmediri@gmail.com>
2023-05-27 08:07:06 +0200

    Final API NOT

--- app/page.js
+++ app/page.js
@@ -6,14 +6,17 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore";
 import { useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
+import Image from "next/image";
 
 export default function Page() {
-
   const db = firebase.firestore();
   const router = useRouter();
 
+  const [Loading, setLoading] = useState(false);
+
   const [count, setcount] = useState(0);
-  const [countGlo, setcountGlo] = useState(0)
+  const [countGlo, setcountGlo] = useState(0);
   const [log, setlog] = useState(false);
   const [client, setclient] = useState({
     etat: log,
@@ -23,29 +26,59 @@ export default function Page() {
     url: "",
   });
 
+  const Handlesubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const prompt = e.target[0].value;
+      const response = await fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt }),
+      }).then((res) => console.log(res));
+      console.log(response);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      } else {
+        console.log('Pas de contenu JSON dans la réponse');
+      }
+      
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setLoading(false);
+  }
+
   useEffect(() => {
     setlog(!log);
   }, [client]);
 
-  async function LocalCount(){
-    setcount(count+1)
-    const UserData = await db.collection("User").doc(client.uid)
-    const GlobalCount = (await UserData.get()).data()
-    if(GlobalCount.Count){
-      await UserData.update({
-        Count: GlobalCount.Count + 1
-      })
-    }else{
-      await UserData.update({
-        Count: 1
-      })
+  async function LocalCount() {
+    setcount(count + 1);
+    try {
+      const UserData = await db.collection("User").doc(client.uid);
+      const GlobalCount = (await UserData.get()).data();
+      if (GlobalCount.Count) {
+        await UserData.update({
+          Count: GlobalCount.Count + 1,
+        });
+      } else {
+        await UserData.update({
+          Count: 1,
+        });
+      }
+      setcountGlo(GlobalCount.Count + 1);
+    } catch (error) {
+      toast.error("PAS DE COMPTE GLOBAL");
     }
-    setcountGlo(GlobalCount.Count+1)
   }
 
-  async function putdata(){
+  async function putdata() {
     await db.collection("User").doc(client.uid).set({
-      Person: client.Person
+      Person: client.Person,
     });
     const data = await db.collection("User").get();
     console.log(data.docs.map((doc) => console.log(doc.data())));
@@ -60,7 +93,6 @@ export default function Page() {
       email: Auth.currentUser.email,
       url: Auth.currentUser.photoURL,
     });
-    
   }
 
   async function signout() {
@@ -114,13 +146,28 @@ export default function Page() {
           {" "}
           <span className="font-bold">Email</span> : {client.email}{" "}
         </p>
-        <div className="grid grid-cols-2 gap-5" >
-          <button onClick={LocalCount}   className="px-5 bg-indigo-700 py-2 rounded font-bold" >
+        <div className="grid grid-cols-2 gap-5">
+          <button
+            onClick={LocalCount}
+            className="px-5 bg-indigo-700 py-2 rounded font-bold"
+          >
             Local Count : {count}
           </button>
-          <button className="px-5 bg-indigo-700 py-2 rounded font-bold" >
+          <button className="px-5 bg-indigo-700 py-2 rounded font-bold">
             Global Count : {countGlo}
           </button>
+        </div>
+        <div className="grid grid-cols-1 gap-5 " >
+          <form onSubmit={(e) => Handlesubmit(e)} className="flex justify-center items-center gap-5 flex-col" >
+            <input
+              type="text"
+              className="px-5 bg-indigo-700 py-2 rounded font-bold"
+            />
+            <button type="submit" className="px-5 w-[30%] bg-indigo-700 py-2 rounded font-bold">
+              Send
+            </button>
+          </form>
+          <Image width={312} height={1} src="https://avatars.githubusercontent.com/u/96632943?v=4" />
         </div>
       </div>
     </>
@@ -152,5 +199,3 @@ function Avatar(props) {
     </>
   );
 }
-
-
