a5f4b6bbdd253649a9f85fe944fde65ed93f8713
WGlint <a.elmediri@gmail.com>
2023-06-02 13:50:27 +0200

    Another Test

--- app/page.js
+++ app/page.js
@@ -8,7 +8,6 @@ import { getDocs } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 import { toast } from "react-hot-toast";
 import Image from "next/image";
-console.log("log")
 
 export default function Page() {
   const db = firebase.firestore();
